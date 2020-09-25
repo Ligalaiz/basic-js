007b2438a3db1c4d35ac18d455f2768ec8bcc480
Ligalaiz <slavakuz88@gmail.com>
2020-09-25 20:01:25 +0300

    feat: implement hanoi-tower

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,4 @@
-const CustomError = require("../extensions/custom-error");
+const CustomError = require('../extensions/custom-error');
 
 module.exports = function transform(/* arr */) {
   throw new CustomError('Not implemented');
